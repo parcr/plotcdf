2590dd382ad7436a3267e535faf8a4c5945e0d7b
PedroCR <pedro.cortereal@magentakoncept.com>
2026-05-19 18:26:19 +0000

    examples for Binomial and Bernouli

--- examples/binomial.py
+++ examples/binomial.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from scipy.stats import binom
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from plotcdf.discrete import plot
@@ -11,20 +12,29 @@ main_name = os.path.basename(main_file) if main_file else 'output'
 main_name = os.path.splitext(main_name)[0] if main_name else 'output'
 
 
-# the output foder is set to examples/bernoulli_output
+# the output foder is set to examples/binomial_output
 output_folder = os.path.join(os.path.dirname(__file__), main_name + '_output')
 os.makedirs(output_folder, exist_ok=True)
-rv_name = 'bernoulli'
+rv_name = 'binomial'
 
+n=5
 p=0.5
-bernoulli = plot.RV(support=[0, 1], prob=[1-p, p])
-bernoulli = plot.RV(support=[0, 1], prob=[1-p, 1.], is_pmf=False)
+rv=binom(n=n, p=p)
+support=range(int(rv.ppf(0))+1, int(rv.ppf(1)) + 1)
+prob=rv.pmf(support)
+print(f'Support: {list(support)}')
+print(f'Probabilities: {list(prob)}')
+prob_sum=prob.cumsum()
+print(f'Cumulative Probabilities: {list(prob_sum)}')
 
-fig_cdf, ax_cdf=bernoulli.plot_cdf(rv_name=rv_name, save=False)
+binomial = plot.RV(support=support, prob=prob)
+binomial = plot.RV(support=support, prob=prob_sum, is_pmf=False)
+
+fig_cdf, ax_cdf=binomial.plot_cdf(rv_name=rv_name, save=False)
 fig_cdf.savefig(os.path.join(output_folder, f'{rv_name}_cdf.png'), dpi=300)
 
-fig_pmf, ax_pmf=bernoulli.plot_pmf(rv_name=rv_name, save=False)
+fig_pmf, ax_pmf=binomial.plot_pmf(rv_name=rv_name, save=False)
 fig_pmf.savefig(os.path.join(output_folder, f'{rv_name}_pmf.png'), dpi=300)
 
-fig_qt, ax_qt=bernoulli.plot_quantile(rv_name=rv_name, save=False)
+fig_qt, ax_qt=binomial.plot_quantile(rv_name=rv_name, save=False)
 fig_qt.savefig(os.path.join(output_folder, f'{rv_name}_quantile.png'), dpi=300)
